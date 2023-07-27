e8d1bde7fefd3786b5c5d676a654d749f1117e97
SHASHANK G SHARMA <77768375+shashankgsharma@users.noreply.github.com>
2023-07-27 07:04:59 +0530

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -1,6 +1,3 @@
-section .data
-    ; Define variables here if needed
-
 section .bss
     fib_number resq 1 ; Reserve 8 bytes (64 bits) to store the Fibonacci number
 
@@ -12,14 +9,17 @@ _start:
     cmp rdi, 2
     jl invalid_args
 
-    ; Get the value of n from the command-line argument and convert it to an integer
-    mov rsi, [rsi + 8] ; Pointer to the first argument (skip the program name)
+    ; Extract the first command-line argument (argument at index 1) into rsi
+    ; The command-line arguments are stored as pointers to strings in memory.
+    ; The first argument (program name) is at [rsi + 8], and the second argument (n) is at [rsi + 16].
+    mov rsi, [rsi + 16] ; Pointer to the second argument (value of n)
+
+    ; Convert the input string to an integer and store it in rdi (n)
     call parse_integer
 
     ; Check if n is less than 0, if yes, then set the Fibonacci number to 0 and exit
-    cmp rax, 0
+    cmp rdi, 0
     jl done
-    mov rdi, 0 ; n
     mov rax, 0 ; Fibonacci number for n=0
     mov rbx, 1 ; Fibonacci number for n=1
 
@@ -81,14 +81,14 @@ parse_integer:
 print_string:
     ; Function to print a null-terminated string pointed to by rsi
     ; Using syscall 1 for sys_write
-    xor rax, rax    ; syscall number 1 for sys_write
+    mov rax, 1      ; syscall number 1 for sys_write
     mov rdi, 1      ; file descriptor 1 (stdout)
-    mov rdx, 0      ; zero out rdx (as length is 0 for null-terminated strings)
+    xor rdx, rdx    ; zero out rdx (as length is 0 for null-terminated strings)
     .print_loop:
         lodsb       ; load the next byte from rsi into al and increment rsi
         test al, al ; check if we reached the end of the string (null terminator)
         jz .print_done
-        mov rdx, 1  ; number of bytes to write (1 byte for each character)
+        inc rdx      ; number of bytes to write (1 byte for each character)
         syscall
         jmp .print_loop
     .print_done:
@@ -97,7 +97,6 @@ print_string:
 print_integer:
     ; Function to print the integer in rdi
     ; Using syscall 1 for sys_write
-    xor rcx, rcx    ; Initialize a counter to keep track of the number of digits
     mov rbx, 10     ; Set rbx to 10 for dividing the number by 10
     mov rsi, output_buffer + output_buffer_size - 1 ; Set rsi to the end of the output buffer
 
@@ -107,24 +106,42 @@ print_integer:
         div rbx      ; Divide rdi by 10, quotient in rax, remainder in rdx
         add dl, '0'  ; Convert the remainder (0-9) to ASCII character ('0'-'9')
         mov [rsi], dl ; Store the ASCII character in the output buffer
-        inc rcx      ; Increment the digit counter
 
         test rax, rax ; Check if quotient is zero
         jnz .convert_digit_loop ; If not zero, continue converting the next digit
 
     ; Print the integer
-    mov rdx, rcx    ; Set the number of digits to print
+    mov rdx, rsi    ; Set the pointer to the beginning of the converted integer
+    sub rdx, rsi    ; Calculate the length of the converted integer
     mov rax, 1      ; syscall number 1 for sys_write
     mov rdi, 1      ; file descriptor 1 (stdout)
-    mov rax, 1      ; syscall number 1 for sys_write
-    mov rdi, 1      ; file descriptor 1 (stdout)
-    mov rax, 1      ; syscall number 1 for sys_write
-    mov rdi, 1      ; file descriptor 1 (stdout)
-    mov rsi, rsi    ; pointer to the output buffer
     syscall
     ret
 
 print_newline:
     ; Function to print a newline character
     ; Using syscall 1 for sys_write
-    xor rax, rax    ; syscall
+    mov rax, 1      ; syscall number 1 for sys_write
+    mov rdi, 1      ; file descriptor 1 (stdout)
+    mov rsi, newline
+    mov rdx, 1      ; number of bytes to write (1 byte for the newline character)
+    syscall
+    ret
+
+exit_program:
+    ; Function to exit the program
+    ; Using syscall 60 for sys_exit
+    mov rax, 60     ; syscall number 60 for sys_exit
+    xor edi, edi    ; Exit status 0
+    syscall
+
+section .rodata
+    prompt db "Enter the value of n: ", 0
+    result_prompt db "The Fibonacci number is: ", 0
+    newline db 10, 0
+
+section .bss
+    invalid_args_msg db "Invalid command-line arguments. Please provide a valid integer for n.", 0
+    input_buffer resb 20 ; Reserve 20 bytes for user input
+    output_buffer resb 21 ; Reserve 21 bytes for output (20 digits + null terminator)
+    input
