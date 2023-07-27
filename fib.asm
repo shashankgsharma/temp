a0188143bce2ac8d76724dff9e8354ec1e8c3c79
SHASHANK G SHARMA <77768375+shashankgsharma@users.noreply.github.com>
2023-07-27 07:35:35 +0530

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -1,8 +1,74 @@
+section .data
+    n_value dq 10 ; Set the value of n here
+
+section .bss
+    fib_number resq 1 ; Reserve 8 bytes (64 bits) to store the Fibonacci number
+    output_buffer resb 101 ; Reserve 101 bytes for output (100 digits + sign + null terminator)
+
+section .text
+    global _start
+
+_start:
+    ; Move the value of 'n' from the data section to rdi (n)
+    mov rdi, qword [n_value]
+
+    ; Check if n is less than 0, if yes, then set the Fibonacci number to 0 and exit
+    cmp rdi, 0
+    jl done
+    mov rax, 0 ; Fibonacci number for n=0
+    mov rbx, 1 ; Fibonacci number for n=1
+
+fib_loop:
+    cmp rdi, 1 ; Check if n is 1 or more
+    jle done   ; If n<=1, then the Fibonacci number is already in rax
+
+    ; Calculate the next Fibonacci number (rax + rbx) and store it in rax
+    add rax, rbx
+
+    ; Swap values: rax (current) -> rbx (prev_1) -> rdx (prev_2) -> rax (current)
+    mov rdx, rbx
+    mov rbx, rax
+    mov rax, rdx
+
+    dec rdi    ; Decrement n
+    jmp fib_loop ; Repeat the loop to calculate the next Fibonacci number
+
+done:
+    ; Store the final Fibonacci number (in rax) into the memory location fib_number
+    mov [fib_number], rax
+
+    ; Print the Fibonacci number
+    call print_newline
+    mov rsi, result_prompt
+    call print_string
+    mov rdi, [fib_number]
+    call print_integer
+    call print_newline
+
+    ; Exit the program
+    call exit_program
+
+print_string:
+    ; Function to print a null-terminated string pointed to by rsi
+    ; Using syscall 1 for sys_write
+    mov rax, 1      ; syscall number 1 for sys_write
+    mov rdi, 1      ; file descriptor 1 (stdout)
+    xor rdx, rdx    ; zero out rdx (as length is 0 for null-terminated strings)
+    .print_loop:
+        lodsb       ; load the next byte from rsi into al and increment rsi
+        test al, al ; check if we reached the end of the string (null terminator)
+        jz .print_done
+        inc rdx      ; number of bytes to write (1 byte for each character)
+        syscall
+        jmp .print_loop
+    .print_done:
+        ret
+
 print_integer:
     ; Function to print the signed integer in rdi
     ; Using syscall 1 for sys_write
-    mov rsi, output_buffer + 20 ; Set rsi to the end of the output buffer (reserve 20 bytes for digits)
-    mov rcx, 20     ; Set the maximum number of digits to print
+    mov rsi, output_buffer + 100 ; Set rsi to the end of the output buffer (reserve 100 bytes for digits)
+    mov rcx, 100     ; Set the maximum number of digits to print
     xor rdx, rdx    ; Clear rdx for handling negative numbers
 
     ; Check if the number is zero
@@ -54,10 +120,37 @@ print_integer:
 
 .print_done:
     ; Print the number
-    mov rdx, 21     ; Set the number of bytes to write (including null terminator)
+    mov rdx, 101     ; Set the number of bytes to write (including null terminator)
     mov rax, 1      ; syscall number 1 for sys_write
     mov rdi, 1      ; file descriptor 1 (stdout)
     syscall
     ret
 
 .print_zero:
+    ; Print "0" for zero
+    mov byte [rsi], '0'
+    inc rsi
+    mov byte [rsi], 0
+    jmp .print_done
+
+print_newline:
+    ; Function to print a newline character
+    ; Using syscall 1 for sys_write
+    mov rax, 1      ; syscall number 1 for sys_write
+    mov rdi, 1      ; file descriptor 1 (stdout)
+    mov rsi, newline
+    mov rdx, 1      ; number of bytes to write (1 byte for the newline character)
+    syscall
+    ret
+
+exit_program:
+    ; Function to exit the program
+    ; Using syscall 60 for sys_exit
+    mov rax, 60     ; syscall number 60 for sys_exit
+    xor edi, edi    ; Exit status 0
+    syscall
+
+section .rodata
+    prompt db "The value of n is: ", 0
+    result_prompt db "The Fibonacci number is: ", 0
+    newline db 10, 
