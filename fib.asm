736eb54cb5692d5ef8bda878727f990a16c1fe09
SHASHANK G SHARMA <77768375+shashankgsharma@users.noreply.github.com>
2023-07-27 07:15:39 +0530

    Update fib.asm

--- fib.asm
+++ fib.asm
@@ -1,21 +1,16 @@
+section .data
+    n_value dq 10 ; Set the value of n here
+
 section .bss
     fib_number resq 1 ; Reserve 8 bytes (64 bits) to store the Fibonacci number
+    output_buffer resb 21 ; Reserve 21 bytes for output (20 digits + null terminator)
 
 section .text
     global _start
 
 _start:
-    ; Check if there is at least one command-line argument (argument count >= 2)
-    cmp rdi, 2
-    jl invalid_args
-
-    ; Extract the first command-line argument (argument at index 1) into rsi
-    ; The command-line arguments are stored as pointers to strings in memory.
-    ; The first argument (program name) is at [rsi + 8], and the second argument (n) is at [rsi + 16].
-    mov rsi, [rsi + 16] ; Pointer to the second argument (value of n)
-
-    ; Convert the input string to an integer and store it in rdi (n)
-    call parse_integer
+    ; Move the value of 'n' from the data section to rdi (n)
+    mov rdi, qword [n_value]
 
     ; Check if n is less than 0, if yes, then set the Fibonacci number to 0 and exit
     cmp rdi, 0
@@ -53,31 +48,6 @@ done:
     ; Exit the program
     call exit_program
 
-invalid_args:
-    ; Print an error message for invalid command-line arguments
-    mov rsi, invalid_args_msg
-    call print_string
-
-    ; Exit the program
-    call exit_program
-
-parse_integer:
-    ; Function to convert a null-terminated string to an integer
-    ; The input string should be in rsi, and the result will be in rax
-    xor rax, rax    ; Clear rax to store the result
-    xor rcx, rcx    ; Clear rcx to use as a counter
-    .convert_loop:
-        movzx rbx, byte [rsi + rcx] ; Load the next byte into rbx
-        cmp rbx, 0   ; Check if we reached the end of the string (null terminator)
-        je .conversion_done
-        sub rbx, '0' ; Convert ASCII character to its integer value
-        imul rax, 10 ; Multiply current result by 10
-        add rax, rbx ; Add the new digit to the result
-        inc rcx      ; Move to the next character
-        jmp .convert_loop
-    .conversion_done:
-        ret
-
 print_string:
     ; Function to print a null-terminated string pointed to by rsi
     ; Using syscall 1 for sys_write
@@ -98,8 +68,43 @@ print_integer:
     ; Function to print the integer in rdi
     ; Using syscall 1 for sys_write
     mov rbx, 10     ; Set rbx to 10 for dividing the number by 10
-    mov rsi, output_buffer + output_buffer_size - 1 ; Set rsi to the end of the output buffer
+    mov rsi, output_buffer + 20 ; Set rsi to the end of the output buffer
 
     .convert_digit_loop:
         dec rsi      ; Move the pointer to the left
-        xor rdx, rdx
+        xor rdx, rdx ; Clear rdx for division
+        div rbx      ; Divide rdi by 10, quotient in rax, remainder in rdx
+        add dl, '0'  ; Convert the remainder (0-9) to ASCII character ('0'-'9')
+        mov [rsi], dl ; Store the ASCII character in the output buffer
+
+        test rax, rax ; Check if quotient is zero
+        jnz .convert_digit_loop ; If not zero, continue converting the next digit
+
+    ; Print the integer
+    mov rdx, 21     ; Set the number of bytes to write (including null terminator)
+    mov rax, 1      ; syscall number 1 for sys_write
+    mov rdi, 1      ; file descriptor 1 (stdout)
+    syscall
+    ret
+
+print_newline:
+    ; Function to print a newline character
+    ; Using syscall 1 for sys_write
+    mov rax, 1      ; syscall number 1 for sys_write
+    mov rdi, 1      ; file descriptor 1 (stdout)
+    mov rsi, newline
+    mov rdx, 1      ; number of bytes to write (1 byte for the newline character)
+    syscall
+    ret
+
+exit_program:
+    ; Function to exit the program
+    ; Using syscall 60 for sys_exit
+    mov rax, 60     ; syscall number 60 for sys_exit
+    xor edi, edi    ; Exit status 0
+    syscall
+
+section .rodata
+    prompt db "The value of n is: ", 0
+    result_prompt db "The Fibonacci number is: ", 0
+    newline db 10, 0
